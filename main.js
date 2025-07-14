09a82521b24921f3320fca3dc3516b5fe651f4dd
gabrielfkm <gabrielhfkm@gmail.com>
2025-07-14 13:20:46 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,23 @@
+import { ProjetoController } from './controllers/ProjetoController.js';
+import { TarefaController } from './controllers/TarefaController.js';
 
+document.addEventListener('DOMContentLoaded', () => {
+  const urlParams = new URLSearchParams(window.location.search);
+  const projetoId = urlParams.get('id');
+
+  const listaProjetos = document.getElementById('lista-projetos');
+  const listaTarefas = document.getElementById('lista-tarefas');
+
+  const projetoController = new ProjetoController(listaProjetos, listaTarefas);
+  projetoController.carregarProjetos().then(() => {
+    if (projetoId) {
+      projetoController.exibirProjeto(projetoId);
+    }
+  });
+
+  if (document.getElementById('form-nova-tarefa')) {
+    const tarefaController = new TarefaController();
+    tarefaController.inicializarFormulario();
+  }
+});
+      
