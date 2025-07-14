34fad0c0cc720f243d9e57a4f4921a4adc059bbf
gabrielfkm <gabrielhfkm@gmail.com>
2025-07-14 13:28:59 -0300

    Update ProjectController.js

--- controllers/ProjectController.js
+++ controllers/ProjectController.js
@@ -1,1 +1,28 @@
+import { ProjetoModel } from '../models/ProjetoModel.js';
+import { ProjetoView } from '../views/ProjetoView.js';
+import { TarefaModel } from '../models/TarefaModel.js';
+import { TarefaView } from '../views/TarefaView.js';
 
+export class ProjetoController {
+  constructor(containerProjetos, containerTarefas) {
+    this.containerProjetos = containerProjetos;
+    this.containerTarefas = containerTarefas;
+  }
+
+  async carregarProjetos() {
+    const projetos = await ProjetoModel.listarProjetos();
+    ProjetoView.renderizarProjetos(projetos, this.containerProjetos);
+
+    if (projetos.length === 0) {
+      ProjetoView.renderizarMensagemSemTarefas(this.containerTarefas);
+    }
+  }
+
+  async exibirProjeto(id) {
+    const projeto = await ProjetoModel.obterProjeto(id);
+    ProjetoView.renderizarDetalhes(projeto);
+
+    const tarefas = await TarefaModel.listarPorProjeto(id);
+    TarefaView.renderizarTarefas(tarefas, document.getElementById('lista-tarefas'));
+  }
+}
