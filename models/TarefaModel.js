db6e10e76617f367514742e16a618567d6cfc8a2
gabrielfkm <gabrielhfkm@gmail.com>
2025-07-14 13:24:48 -0300

    Update TarefaModel.js

--- models/TarefaModel.js
+++ models/TarefaModel.js
@@ -1,1 +1,30 @@
+const BASE_URL = "https://6875284bdd06792b9c96fd2e.mockapi.io/project/API";
 
+export class TarefaModel {
+  static async listarPorProjeto(projetoId) {
+    const res = await fetch(`${BASE_URL}/tarefas?projetoId=${projetoId}`);
+    return res.json();
+  }
+
+  static async adicionar(tarefa) {
+    const res = await fetch(`${BASE_URL}/tarefas`, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(tarefa)
+    });
+    return res.json();
+  }
+
+  static async atualizar(id, dados) {
+    const res = await fetch(`${BASE_URL}/tarefas/${id}`, {
+      method: 'PUT',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(dados)
+    });
+    return res.json();
+  }
+
+  static async excluir(id) {
+    await fetch(`${BASE_URL}/tarefas/${id}`, { method: 'DELETE' });
+  }
+}
